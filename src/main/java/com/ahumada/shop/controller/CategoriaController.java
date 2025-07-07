ae0cedfeb3c763a025f1c317dc67cb74072345d4
Mario Ahumada <ahumadamob@gmail.com>
2025-07-06 22:37:58 -0300

    Rename controller to CategoriaController

--- src/main/java/com/ahumada/shop/controller/CategoriaController.java
+++ src/main/java/com/ahumada/shop/controller/CategoriaController.java
@@ -15,7 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/categorias")
 @RequiredArgsConstructor
-public class CategoryController {
+public class CategoriaController {
 
     private final ICategoriaService categoriaService;
     private final CategoriaMapper categoriaMapper;
